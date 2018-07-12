d594aae6fd9f91a1c74dc7ad68241deb43a03cf3
vivek <dubeyvivek74@gmail.com>
2018-07-12 21:56:52 +0530

    Commit

---
+++ src/com/pack/F1.java
@@ -0,0 +1,11 @@
+package com.pack;
+
+import org.testng.annotations.Test;
+
+public class F1 {
+	@Test
+	public void hike(){
+		System.out.println("hello1");
+	}
+
+}
---
+++ src/com/pack/F2.java
@@ -0,0 +1,12 @@
+package com.pack;
+
+import org.testng.annotations.Test;
+
+public class F2 {
+	@Test
+	public void Viber(){
+		System.out.println("hello2");
+	}
+
+
+}
